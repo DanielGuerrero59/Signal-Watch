8975d91f704e7f503baca8ba82c4220151b622ab
DanielG <danielguerrero562006@gmail.com>
2026-02-22 21:26:11 -0500

    Initial commit with backend and docker setup

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI  
+# fastapi is the module(toolbox), 
+# from keyword in python is typically used to import specific items from a module 
+# we are importing the FastAPI class from the fastapi module/TOOLBOX
+
+app = FastAPI(title="SignalWatch")  # creates actual server object, app is our instance 
+# the constructor has optional arguments, and we use the keyword way of assigning the value directly 
+# possible in python ^^^
+
+
+@app.get("/health")  # decorator function (start again tomorrow) 
+#.get() is a built in function from fastapi to handle get requests from HTTP
+
+
+def health_check():   #standard python function syntax 
+    return {"status": "ok"}         
