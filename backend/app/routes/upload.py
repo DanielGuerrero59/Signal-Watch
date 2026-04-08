06f3ce5206d90ade79ec245aeef6c09b18d89756
DanielG <danielguerrero562006@gmail.com>
2026-04-07 20:28:28 -0400

    upload.py

---
+++ backend/app/routes/upload.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from fastapi import APIRouter 
+
+
+class UploadRequest(BaseModel): 
+    filename: str 
+    size: int 
+    description: str
+
+
+router = APIRouter()
+
+@router.post("/upload", status_code = 201) #https status code 201 to create something 
+def upload_file(request: UploadRequest):
+    return { 
+        "received" : request.filename, 
+        "size" : request.size, 
+        "description" : request.description
+    }
+
+
+
