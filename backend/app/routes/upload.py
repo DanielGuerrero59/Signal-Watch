4d636b80643c4b9ae1e393049a437572ce86584f
DanielG <danielguerrero562006@gmail.com>
2026-04-10 20:59:47 -0400

    Upload.py accepting files

--- backend/app/routes/upload.py
+++ backend/app/routes/upload.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from fastapi import APIRouter 
+from fastapi import UploadFile, File 
 
 class UploadRequest(BaseModel): 
     filename: str 
@@ -10,11 +11,14 @@ class UploadRequest(BaseModel):
 router = APIRouter()
 
 @router.post("/upload", status_code = 201) #https status code 201 to create something 
-def upload_file(request: UploadRequest):
+async def upload_file(file: UploadFile):
+    # .read() reads through file in form of bytes 
+    #await makes program wait to read the file before continuing 
+    contents = await file.read() 
     return { 
-        "received" : request.filename, 
-        "size" : request.size, 
-        "description" : request.description
+        "Filename": file.filename, 
+        "Size" : len(contents)
+
     }
 
 
