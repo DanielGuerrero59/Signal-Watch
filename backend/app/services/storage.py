6a702e26b3bc78661240bbc606d13fba15981179
DanielG <danielguerrero562006@gmail.com>
2026-06-23 22:20:04 -0400

    Error handling of Type and OS clear egkng ge seg gse s s s s clear

--- backend/app/services/storage.py
+++ backend/app/services/storage.py
@@ -1,5 +1,7 @@
 import os
+from fastapi import HTTPException
 from dotenv import load_dotenv
+
 
 # reading copying every line of our env file to python
 load_dotenv()
@@ -7,18 +9,27 @@ load_dotenv()
 def save_file(filename: str, contents: bytes):
     # os.makedirs creates the uploads folder if it doesn't exist
     # exist_ok=True means don't crash if the folder is already there — just continue
+    try: 
+        # using key value pair to get our value of upload folder 
+        os.makedirs(os.getenv("UPLOAD_DIR"), exist_ok=True)
 
-    # using key value pair to get our value of upload folder 
-    os.makedirs(os.getenv("UPLOAD_DIR"), exist_ok=True)
+    
+    
 
-    # Builds the full path where the file will be saved e.g. "uploads/report.pdf"
-    path = f"{os.getenv('UPLOAD_DIR')}/{filename}"
 
-    # open(path, "wb") opens the file at that path in write-bytes mode
-    # "wb" = write bytes — needed because files are binary data, not plain text
-    # the "with" keyword automatically closes the file when we're done — no cleanup needed
-    with open(path, "wb") as f:
-        f.write(contents)  # Writes the raw bytes to disk
+        # Builds the full path where the file will be saved e.g. "uploads/report.pdf"
+        path = f"{os.getenv('UPLOAD_DIR')}/{filename}"
+
+        # open(path, "wb") opens the file at that path in write-bytes mode
+        # "wb" = write bytes — needed because files are binary data, not plain text
+        # the "with" keyword automatically closes the file when we're done — no cleanup needed
+        with open(path, "wb") as f:
+            f.write(contents)  # Writes the raw bytes to disk
 
     # Returns the path so the caller knows where the file ended up
+    except TypeError: 
+        raise HTTPException(status_code = 500, detail = "Server misconfigured: UPLOAD_DIR not set")
+
+    except OSError: 
+        raise HTTPException(status_code = 500, detail = "Failed to save file: server filesystem error")
     return path
